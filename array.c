4abc51e252346c6388965f0b8a14d75659a4eb2a
saurabhy23 <107410370+saurabhy23@users.noreply.github.com>
2022-06-13 18:13:06 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	int n=1;
+	int a[n];
+	int b[n];
+	printf("enter the no. of elements\n");
+	scanf("%d",&n);
+	printf("enter the elements of 1st array\n");
+		for(int i=0;i<n;i++){
+			scanf("%d",&a[i]);
+		}
+		printf("enter the elements of 2nd array\n");
+		for(int i=0;i<n;i++){
+			scanf("%d",&b[i]);
+		}
+		for(int i=0;i<n;i++){
+			if(a[i]!=b[i]){
+			printf("arrays don't have same elements\n");
+			break;	
+			}
+			if(i==(n-1)&&a[i]==b[i])
+			printf("arrays have same elements\n");
+		}
+}
